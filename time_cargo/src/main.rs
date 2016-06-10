6d6ad8eda408a43a229872bced2a2b40923f0462
Niko Matsakis <niko@alum.mit.edu>
2016-06-10 13:31:26 -0400

    extend with various features

--- time_cargo/src/main.rs
+++ time_cargo/src/main.rs
@@ -1,27 +1,69 @@
 extern crate cargo;
+extern crate docopt;
 extern crate git2;
 extern crate walkdir;
 extern crate libc;
+extern crate rustc_serialize;
 
 use std::env;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::io;
 use std::os::unix::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::time::Instant;
 
 use cargo::core::{Source, SourceId, Registry, Dependency};
 use cargo::ops;
 use cargo::sources::RegistrySource;
 use cargo::core::shell::{Shell, MultiShell, Verbosity, ShellConfig, ColorConfig};
 use cargo::util::Config;
+use docopt::Docopt;
+use rustc_serialize::Decodable;
 use walkdir::{WalkDir, DirEntry, WalkDirIterator};
 
+// Write the Docopt usage string.
+const USAGE: &'static str = r#"
+Usage:
+  time_cargo build [options] <package-name>...
+  time_cargo test [options] <package-name>...
+
+Builds or tests the latest version of packages from crates.io, saving
+timing information and other results. If the special package-name "*"
+is used, we will test all packages. (Use `'*'` to prevent your shell
+from expanding wildcards.)
+
+WARNING: Building or testing packages from crates.io involves executing
+arbitary code! Be wary.
+
+Options:
+  -h, --help  Show this screen.
+  -o DIR      Output directory [default: out].
+"#;
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    cmd_build: bool,
+    cmd_test: bool,
+    flag_help: bool,
+    flag_o: String,
+    arg_package_name: Vec<String>,
+}
+
 fn main() {
-    if fs::metadata("index").is_err() {
+    let args: Args =
+        Docopt::new(USAGE)
+        .and_then(|d| d.argv(env::args()).decode())
+        .unwrap_or_else(|e| e.exit());
+
+    let root = PathBuf::from(args.flag_o.clone());
+
+    let index = root.join("index");
+    if fs::metadata(&index).is_err() {
+        let dot_index = root.join(".index");
         git2::Repository::clone("https://github.com/rust-lang/crates.io-index",
-                                ".index").unwrap();
-        fs::rename(".index", "index").unwrap();
+                                &dot_index).unwrap();
+        fs::rename(&dot_index, &index).unwrap();
     }
 
     let config = config();
@@ -33,12 +75,19 @@ fn main() {
     let stderr = unsafe { libc::dup(2) };
     assert!(stdout > 0 && stderr > 0);
 
-    let root = env::current_dir().unwrap();
-    for krate in WalkDir::new("index").into_iter()
-                        .filter_entry(|e| !bad(e))
-                        .filter_map(|e| e.ok())
-                        .filter(|e| e.file_type().is_file())
-                        .map(|e| e.file_name().to_str().unwrap().to_string()) {
+    let krate_names = if args.arg_package_name.iter().any(|s| s == "*") {
+        WalkDir::new(&index)
+            .into_iter()
+            .filter_entry(|e| !bad(e))
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .map(|e| e.file_name().to_str().unwrap().to_string())
+            .collect()
+    } else {
+        args.arg_package_name.clone()
+    };
+
+    for krate in krate_names {
         //FIXME: Skip crates this script has trouble with, but ultimately
         //we should figure out why and include them
         if krate == "cosmo" { continue; }
@@ -48,9 +97,10 @@ fn main() {
         if krate == "simple" { continue; }
         let root_output = root.join("output").join(&krate);
         if fs::metadata(root.join("output").join(&krate).join("stdio")).is_ok() {
+            println!("skipping {}", krate);
             continue
         }
-        build(&root_output, &mut s, &id, &krate);
+        build_or_test(&args, &root, &root_output, &mut s, &id, &krate);
         io::stdout().flush().unwrap();
         unsafe {
             assert_eq!(libc::dup2(stdout, 1), 1);
@@ -77,7 +127,12 @@ fn config() -> Config {
         env::current_dir().unwrap(), env::home_dir().unwrap()).unwrap()
 }
 
-fn build(out: &Path, src: &mut RegistrySource, id: &SourceId, krate: &str) {
+fn build_or_test(args: &Args,
+                 root: &Path,
+                 out: &Path,
+                 src: &mut RegistrySource,
+                 id: &SourceId,
+                 krate: &str) {
     println!("working on: {}", krate);
     fs::create_dir_all(&out).unwrap();
     unsafe {
@@ -97,49 +152,62 @@ fn build(out: &Path, src: &mut RegistrySource, id: &SourceId, krate: &str) {
     };
 
     let pkg = match src.download(&pkg) {
-        Ok(mut v) => v,
+        Ok(v) => v,
         Err(e) => {
             return println!("bad get pkg: {}: {}", pkg, e);
         }
     };
 
     fs::create_dir_all(".cargo").unwrap();
-    File::create(".cargo/config").unwrap().write_all("
+    File::create(".cargo/config").unwrap().write_all(format!("
         [build]
-        target-dir = '/home/ec2-user/versioned/cratesio_perf/time_cargo/results'
-    ".as_bytes()).unwrap();
+        target-dir = '{}'
+    ", root.join("results").display()).as_bytes()).unwrap();
 
-    let config = config();
-    let args = &["-Z".to_string(), "time-passes".to_string()];
-    let res = ops::run_tests(&pkg, None, &ops::TestOptions {
-        compile_opts: CompileOptions {
-            config: &config,
-            jobs: None,
-            target: None,
-            features: &[],
-            no_default_features: false,
-            spec: &[],
-            filter: ops::CompileFilter::Only {
-                lib: true,
-                examples: &[],
-                bins: &[],
-                tests: &[],
-                benches: &[],
-            },
-            exec_engine: None,
-            release: true,
-            mode: ops::CompileMode::Build,
-            target_rustc_args: Some(args),
-            target_rustdoc_args: None
+    let rustc_args = &["-Z".to_string(), "time-passes".to_string()];
+
+    let compile_opts = ops::CompileOptions {
+        config: &config(),
+        jobs: None,
+        target: None,
+        features: &[],
+        no_default_features: false,
+        spec: &[],
+        filter: ops::CompileFilter::Only {
+            lib: true,
+            examples: &[],
+            bins: &[],
+            tests: &[],
+            benches: &[],
         },
-        no_run: false,
-        no_fail_fast: false,
-        ony_doc: false});
-    fs::remove_file(".cargo/config").unwrap();
-    if let Err(e) = res {
-        println!("bad compile {}: {} {:?}", pkg, e, e.cause());
-    } else {
-        println!("OK");
+        exec_engine: None,
+        release: true,
+        mode: ops::CompileMode::Build,
+        target_rustc_args: Some(rustc_args),
+        target_rustdoc_args: None
+    };
+
+    match ops::compile_pkg(&pkg, None, &compile_opts) {
+        Ok(_) => println!("> compile passed for `{}`", pkg),
+        Err(e) => println!("> compile failed for `{}`: {}", pkg, e)
+    }
+
+    if args.cmd_test {
+        let test_opts = ops::TestOptions {
+            compile_opts: compile_opts,
+            no_run: false,
+            no_fail_fast: false
+        };
+
+        let start = Instant::now();
+        let result = ops::run_tests(pkg.manifest_path(), &test_opts, &[]);
+        let test_time = start.elapsed();
+
+        match result {
+            Ok(None) => println!("> tests passed for `{}`: {:?}", pkg, test_time),
+            Ok(Some(err)) => println!("> tests failed for `{}`: {}", pkg, err),
+            Err(err) => println!("> cargo error for `{}`: {}", pkg, err),
+        }
     }
 }
 
