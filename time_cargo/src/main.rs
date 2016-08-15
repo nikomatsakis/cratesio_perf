00272b62e6b1812e7c1ebfeefa2acd36a17be42f
Niko Matsakis <niko@alum.mit.edu>
2016-08-15 12:30:25 -0400

    add support for custom version numbers

--- time_cargo/src/main.rs
+++ time_cargo/src/main.rs
@@ -3,15 +3,8 @@ extern crate docopt;
 extern crate git2;
 extern crate walkdir;
 extern crate libc;
+extern crate regex;
 extern crate rustc_serialize;
-
-use std::env;
-use std::fs::{self, File};
-use std::io::prelude::*;
-use std::io;
-use std::os::unix::prelude::*;
-use std::path::{Path, PathBuf};
-use std::time::Instant;
 
 use cargo::core::{Source, SourceId, Registry, Dependency};
 use cargo::ops;
@@ -19,7 +12,16 @@ use cargo::sources::RegistrySource;
 use cargo::core::shell::{Shell, MultiShell, Verbosity, ShellConfig, ColorConfig};
 use cargo::util::Config;
 use docopt::Docopt;
-use rustc_serialize::Decodable;
+use regex::Regex;
+use std::env;
+use std::fmt;
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io;
+use std::os::unix::prelude::*;
+use std::path::{Path, PathBuf};
+use std::process;
+use std::time::Instant;
 use walkdir::{WalkDir, DirEntry, WalkDirIterator};
 
 // Write the Docopt usage string.
@@ -52,9 +54,13 @@ struct Args {
     arg_package_name: Vec<String>,
 }
 
+struct KrateName {
+    name: String,
+    version: Option<String>,
+}
+
 fn main() {
-    let args: Args =
-        Docopt::new(USAGE)
+    let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.argv(env::args()).decode())
         .unwrap_or_else(|e| e.exit());
 
@@ -63,8 +69,8 @@ fn main() {
     let index = root.join("index");
     if fs::metadata(&index).is_err() {
         let dot_index = root.join(".index");
-        git2::Repository::clone("https://github.com/rust-lang/crates.io-index",
-                                &dot_index).unwrap();
+        git2::Repository::clone("https://github.com/rust-lang/crates.io-index", &dot_index)
+            .unwrap();
         fs::rename(&dot_index, &index).unwrap();
     }
 
@@ -77,30 +83,63 @@ fn main() {
     let stderr = unsafe { libc::dup(2) };
     assert!(stdout > 0 && stderr > 0);
 
-    let krate_names = if args.arg_package_name.iter().any(|s| s == "*") {
+    let krate_names: Vec<KrateName> = if args.arg_package_name.iter().any(|s| s == "*") {
         WalkDir::new(&index)
             .into_iter()
             .filter_entry(|e| !bad(e))
             .filter_map(|e| e.ok())
             .filter(|e| e.file_type().is_file())
-            .map(|e| e.file_name().to_str().unwrap().to_string())
+            .map(|e| {
+                KrateName {
+                    name: e.file_name().to_str().unwrap().to_string(),
+                    version: None,
+                }
+            })
             .collect()
     } else {
-        args.arg_package_name.clone()
+        let regex = Regex::new(r"\s*([^=\s]+)\s*(?:=\s*[^=\s]+)?").unwrap();
+        args.arg_package_name
+            .iter()
+            .map(|str| {
+                match regex.captures(&str) {
+                    Some(captures) => {
+                        KrateName {
+                            name: captures.at(1).unwrap().to_string(),
+                            version: captures.at(2).map(|s| s.to_string()),
+                        }
+                    }
+                    None => {
+                        println!("invalid package name / version `{}`, try `foo` or `foo=0.1`",
+                                 str);
+                        process::exit(1)
+                    }
+                }
+            })
+            .collect()
     };
 
     for krate in krate_names {
-        //FIXME: Skip crates this script has trouble with, but ultimately
-        //we should figure out why and include them
-        if krate == "cosmo" { continue; }
-        if krate == "gfx_text" { continue; }
-        if krate == "parasailors" { continue; }
-        if krate == "parasail-sys" { continue; }
-        if krate == "simple" { continue; }
-        let root_output = root.join("output").join(&krate);
-        if fs::metadata(root.join("output").join(&krate).join("stdio")).is_ok() {
+        // FIXME: Skip crates this script has trouble with, but ultimately
+        // we should figure out why and include them
+        //if krate.name == "cosmo" {
+        //continue;
+        //}
+        if krate.name == "gfx_text" {
+            continue;
+        }
+        if krate.name == "parasailors" {
+            continue;
+        }
+        if krate.name == "parasail-sys" {
+            continue;
+        }
+        if krate.name == "simple" {
+            continue;
+        }
+        let root_output = root.join("output").join(&krate.name);
+        if fs::metadata(root.join("output").join(&krate.name).join("stdio")).is_ok() {
             println!("skipping {}", krate);
-            continue
+            continue;
         }
         build_or_test(&args, &root, &root_output, &mut s, &id, &krate);
         io::stdout().flush().unwrap();
@@ -113,9 +152,9 @@ fn main() {
 
 fn bad(entry: &DirEntry) -> bool {
     entry.file_name()
-         .to_str()
-         .map(|s| s.starts_with(".") || s.ends_with(".json"))
-         .unwrap_or(false)
+        .to_str()
+        .map(|s| s.starts_with(".") || s.ends_with(".json"))
+        .unwrap_or(false)
 }
 
 fn config() -> Config {
@@ -126,7 +165,9 @@ fn config() -> Config {
     let out = Shell::create(Box::new(io::stdout()), config);
     let err = Shell::create(Box::new(io::stderr()), config);
     Config::new(MultiShell::new(out, err, Verbosity::Normal),
-        env::current_dir().unwrap(), env::home_dir().unwrap()).unwrap()
+                env::current_dir().unwrap(),
+                env::home_dir().unwrap())
+        .unwrap()
 }
 
 fn build_or_test(args: &Args,
@@ -134,7 +175,7 @@ fn build_or_test(args: &Args,
                  out: &Path,
                  src: &mut RegistrySource,
                  id: &SourceId,
-                 krate: &str) {
+                 krate: &KrateName) {
     println!("working on: {}", krate);
     fs::create_dir_all(&out).unwrap();
     unsafe {
@@ -143,9 +184,15 @@ fn build_or_test(args: &Args,
         assert_eq!(libc::dup2(stdout.as_raw_fd(), 2), 2);
     }
 
-    let dep = Dependency::parse(krate, None, &id).unwrap();
-    let pkg = src.query(&dep).unwrap().iter().map(|v| v.package_id())
-                 .max().cloned();
+    let dep = Dependency::parse(&krate.name,
+                                krate.version.as_ref().map(|s| &s[..]),
+                                &id).unwrap();
+    let pkg = src.query(&dep)
+        .unwrap()
+        .iter()
+        .map(|v| v.package_id())
+        .max()
+        .cloned();
     let pkg = match pkg {
         Some(pkg) => pkg,
         None => {
@@ -161,10 +208,15 @@ fn build_or_test(args: &Args,
     };
 
     fs::create_dir_all(".cargo").unwrap();
-    File::create(".cargo/config").unwrap().write_all(format!("
+    File::create(".cargo/config")
+        .unwrap()
+        .write_all(format!("
         [build]
         target-dir = '{}'
-    ", root.join("results").display()).as_bytes()).unwrap();
+    ",
+                           root.join("results").display())
+            .as_bytes())
+        .unwrap();
 
     let rustc_args = &["-Z".to_string(), "time-passes".to_string()];
 
@@ -186,18 +238,18 @@ fn build_or_test(args: &Args,
         release: args.flag_release,
         mode: ops::CompileMode::Build,
         target_rustc_args: Some(rustc_args),
-        target_rustdoc_args: None
+        target_rustdoc_args: None,
     };
 
     let test_opts = ops::TestOptions {
         compile_opts: compile_opts,
         no_run: false,
-        no_fail_fast: false
+        no_fail_fast: false,
     };
 
     match ops::compile_pkg(&pkg, None, &test_opts.compile_opts) {
         Ok(_) => println!("> compile passed for `{}`", pkg),
-        Err(e) => println!("> compile failed for `{}`: {}", pkg, e)
+        Err(e) => println!("> compile failed for `{}`: {}", pkg, e),
     }
 
     if args.flag_test {
@@ -225,3 +277,12 @@ fn build_or_test(args: &Args,
     }
 }
 
+impl fmt::Display for KrateName {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        if let Some(ref ver) = self.version {
+            write!(fmt, "{}={}", self.name, ver)
+        } else {
+            write!(fmt, "{}", self.name)
+        }
+    }
+}
