b338f3dcd54261f942bb3d0ac4e1b2e8b064b35a
Niko Matsakis <niko@alum.mit.edu>
2016-08-15 14:49:35 -0400

    use Box<Error> and thread things through a lot more

--- time_cargo/src/main.rs
+++ time_cargo/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(question_mark)]
+
 extern crate cargo;
 extern crate docopt;
 extern crate git2;
@@ -14,6 +16,7 @@ use cargo::util::Config;
 use docopt::Docopt;
 use regex::Regex;
 use std::env;
+use std::error::Error;
 use std::fmt;
 use std::fs::{self, File};
 use std::io::prelude::*;
@@ -41,7 +44,9 @@ Options:
   -o <dir>, --out <dir>  Output directory [default: out].
   -t, --test             Run tests.
   -b, --bench            Run benchmarks.
-  -r, --release          Use release mode instead of debug.
+  --release              Use release mode instead of debug.
+  --force                Delete results left-over from prior runs.
+  --stop-on-error        Stop if an error results from processing a crate.
 "#;
 
 #[derive(Debug, RustcDecodable)]
@@ -51,9 +56,12 @@ struct Args {
     flag_test: bool,
     flag_bench: bool,
     flag_release: bool,
+    flag_force: bool,
+    flag_stop_on_error: bool,
     arg_package_name: Vec<String>,
 }
 
+#[derive(Clone, Debug)]
 struct KrateName {
     name: String,
     version: Option<String>,
@@ -121,9 +129,9 @@ fn main() {
     for krate in krate_names {
         // FIXME: Skip crates this script has trouble with, but ultimately
         // we should figure out why and include them
-        //if krate.name == "cosmo" {
-        //continue;
-        //}
+        // if krate.name == "cosmo" {
+        // continue;
+        // }
         if krate.name == "gfx_text" {
             continue;
         }
@@ -136,12 +144,18 @@ fn main() {
         if krate.name == "simple" {
             continue;
         }
-        let root_output = root.join("output").join(&krate.name);
-        if fs::metadata(root.join("output").join(&krate.name).join("stdio")).is_ok() {
-            println!("skipping {}", krate);
-            continue;
+        let root_output = root.join("output").join(&krate.to_string());
+        match build_or_test(&args, &root, &root_output, &mut s, &id, &krate) {
+            Ok(()) => {}
+            Err(ref err) => {
+                println!("{}: failed because of `{}`", krate, err);
+
+                if args.flag_stop_on_error {
+                    println!("aborting due to --stop-on-error flag");
+                    process::exit(1);
+                }
+            }
         }
-        build_or_test(&args, &root, &root_output, &mut s, &id, &krate);
         io::stdout().flush().unwrap();
         unsafe {
             assert_eq!(libc::dup2(stdout, 1), 1);
@@ -175,48 +189,52 @@ fn build_or_test(args: &Args,
                  out: &Path,
                  src: &mut RegistrySource,
                  id: &SourceId,
-                 krate: &KrateName) {
-    println!("working on: {}", krate);
-    fs::create_dir_all(&out).unwrap();
+                 krate: &KrateName)
+                 -> Result<(), Box<Error>> {
+    if fs::metadata(out.join("stdio")).is_ok() {
+        if args.flag_force {
+            println!("{}: skipping", krate);
+            return Ok(());
+        } else {
+            println!("{}: removing prior results", krate);
+            fs::remove_dir_all(out)?;
+        }
+    }
+
+    println!("{}: building and storing results in {}",
+             krate,
+             out.display());
+    fs::create_dir_all(&out)?;
     unsafe {
-        let stdout = File::create(out.join("stdio")).unwrap();
+        let stdout = File::create(out.join("stdio"))?;
         assert_eq!(libc::dup2(stdout.as_raw_fd(), 1), 1);
         assert_eq!(libc::dup2(stdout.as_raw_fd(), 2), 2);
     }
 
-    let dep = Dependency::parse(&krate.name,
-                                krate.version.as_ref().map(|s| &s[..]),
-                                &id).unwrap();
-    let pkg = src.query(&dep)
-        .unwrap()
+    let dep = Dependency::parse(&krate.name, krate.version.as_ref().map(|s| &s[..]), &id)?;
+    let pkg = src.query(&dep)?
         .iter()
         .map(|v| v.package_id())
         .max()
         .cloned();
     let pkg = match pkg {
         Some(pkg) => pkg,
-        None => {
-            return println!("failed to find {}", krate);
-        }
+        None => return Err(LocalError::NotInRegistry(krate.clone()).into()),
     };
 
     let pkg = match src.download(&pkg) {
         Ok(v) => v,
-        Err(e) => {
-            return println!("bad get pkg: {}: {}", pkg, e);
-        }
+        Err(e) => return Err(LocalError::FailedToDownload(krate.clone(), e.into()).into()),
     };
 
-    fs::create_dir_all(".cargo").unwrap();
-    File::create(".cargo/config")
-        .unwrap()
+    fs::create_dir_all(".cargo")?;
+    File::create(".cargo/config")?
         .write_all(format!("
         [build]
         target-dir = '{}'
     ",
                            root.join("results").display())
-            .as_bytes())
-        .unwrap();
+            .as_bytes())?;
 
     let rustc_args = &["-Z".to_string(), "time-passes".to_string()];
 
@@ -275,6 +293,8 @@ fn build_or_test(args: &Args,
             Err(err) => println!("> cargo error for `{}`: {}", pkg, err),
         }
     }
+
+    Ok(())
 }
 
 impl fmt::Display for KrateName {
@@ -283,6 +303,42 @@ impl fmt::Display for KrateName {
             write!(fmt, "{}={}", self.name, ver)
         } else {
             write!(fmt, "{}", self.name)
+        }
+    }
+}
+
+#[derive(Debug)]
+enum LocalError {
+    NotInRegistry(KrateName),
+    FailedToDownload(KrateName, Box<Error>),
+}
+
+impl fmt::Display for LocalError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        use LocalError::*;
+        match *self {
+            NotInRegistry(ref k) =>
+                write!(fmt, "crate `{}` not in registry", k),
+            FailedToDownload(ref k, ref e) =>
+                write!(fmt, "crate `{}` failed to download: {}", k, e),
+        }
+    }
+}
+
+impl Error for LocalError {
+    fn description(&self) -> &str {
+        use LocalError::*;
+        match *self {
+            NotInRegistry(..) => "not in registry",
+            FailedToDownload(..) => "failed to download",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        use LocalError::*;
+        match *self {
+            NotInRegistry(..) => None,
+            FailedToDownload(_, ref e) => Some(&**e),
         }
     }
 }
